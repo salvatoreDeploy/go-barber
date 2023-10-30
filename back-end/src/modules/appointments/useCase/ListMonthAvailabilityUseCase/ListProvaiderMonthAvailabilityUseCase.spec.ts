c28fe98558c3e1e09ae4b0e7154b93037e4ae47e
salvatoreDeploy <liderhenrique@gmail.com>
2023-10-30 13:42:42 -0300

    listing available days

--- back-end/src/modules/appointments/useCase/ListMonthAvailabilityUseCase/ListProvaiderMonthAvailabilityUseCase.spec.ts
+++ back-end/src/modules/appointments/useCase/ListMonthAvailabilityUseCase/ListProvaiderMonthAvailabilityUseCase.spec.ts
@@ -1,6 +1,7 @@
-import { beforeEach, describe, expect, it, vi } from "vitest";
+import { beforeEach, describe, expect, it, test, vi } from "vitest";
 import { ListProvaiderMonthAvailabilitysUseCase } from "./ListProvaiderMonthAvailabilityUseCase";
 import { InMemoryAppointmentRepository } from "@modules/appointments/repositories/in-memory/inMemoryAppointmentsRepository";
+import { getHours, getSeconds, setHours, setSeconds } from "date-fns";
 
 let inMemoryAppointmentsRepository: InMemoryAppointmentRepository;
 let sut: ListProvaiderMonthAvailabilitysUseCase;
@@ -14,38 +15,38 @@ describe("List Provaiders Month Availability", () => {
   });
 
   it("should be able to list providers the month availability from provaider", async () => {
-    await inMemoryAppointmentsRepository.create({
-      provider_id: "provaider",
-      date: new Date(2023, 10, 22, 8, 0, 0) /* 22/10/2023 8:00 */,
-    });
+    const teste = [];
+
+    let date = new Date(2023, 10, 22, 5, 0, 0);
+
+    for (let i = 0; i < 10; i++) {
+      let hour = getHours(date);
+      date.setHours(hour + 1);
+
+      const value = await inMemoryAppointmentsRepository.create({
+        provider_id: "provaider",
+        date: new Date(2023, 10, 22, hour, 0, 0) /* 22/10/2023 8:00 */,
+      });
+
+      teste.push(value);
+    }
 
     await inMemoryAppointmentsRepository.create({
       provider_id: "provaider",
-      date: new Date(2023, 10, 22, 10, 0, 0) /* 22/11/2023 10:00 */,
-    });
-
-    await inMemoryAppointmentsRepository.create({
-      provider_id: "provaider",
-      date: new Date(2023, 10, 23, 9, 0, 0) /* 23/11/2023 9:00 */,
-    });
-
-    await inMemoryAppointmentsRepository.create({
-      provider_id: "provaider",
-      date: new Date(2023, 11, 13, 9, 0, 0) /* 13/12/2023 9:00 */,
+      date: new Date(2023, 10, 23, 9, 0, 0) /* 13/12/2023 9:00 */,
     });
 
     const availability = await sut.execute({
       provider_id: "provaider",
-      month: 12,
+      month: 11,
       year: 2023,
     });
 
     expect(availability).toEqual(
       expect.arrayContaining([
-        { day: 1, available: true },
-        { day: 2, available: false },
-        { day: 3, available: false },
-        { day: 4, available: true },
+        { day: 21, available: true },
+        { day: 22, available: false },
+        { day: 23, available: true },
       ])
     );
   });
--- back-end/src/modules/appointments/useCase/ListMonthAvailabilityUseCase/ListProvaiderMonthAvailabilityUseCase.ts
+++ back-end/src/modules/appointments/useCase/ListMonthAvailabilityUseCase/ListProvaiderMonthAvailabilityUseCase.ts
@@ -4,6 +4,7 @@ import { Users } from "@prisma/client";
 import { AppError } from "@shared/error/AppError";
 import { inject, injectable } from "inversify";
 import { IAppointmentsRepository } from "@modules/appointments/repositories/IAppointmentsRepository";
+import { getDate, getDaysInMonth } from "date-fns";
 
 interface IRequest {
   provider_id: string;
@@ -35,7 +36,24 @@ class ListProvaiderMonthAvailabilitysUseCase {
         year,
       });
 
-    return [{ day: 5, available: false }];
+    const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
+
+    const eachDaysArray = Array.from(
+      { length: numberOfDaysInMonth },
+      (_, index) => index + 1
+    );
+
+    const vailability = eachDaysArray.map((day) => {
+      const appointmentsInDay = appointmentsMonth.filter((appointment) => {
+        return getDate(appointment.date) === day;
+      });
+      return {
+        day,
+        available: appointmentsInDay.length < 10,
+      };
+    });
+
+    return vailability;
   }
 }
 
